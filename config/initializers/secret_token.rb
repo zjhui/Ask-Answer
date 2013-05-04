ea79532c353af2c98d56fd5817911f3a5089ef24
zjhui <jiahui.tar.gz@gmail.com>
2013-05-04 22:23:32 +0800

    finished the application.html.erb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AskAnswer::Application.config.secret_token = 'c76f9fe8e96cb83b73c79c3299c5502c7cf63ea211e18fceef20864db5d776497d2baecda32358ad62dcce63595e2d78942bd44c5faa986ef2a7615cf2318542'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AskAnswer::Application.config.session_store :cookie_store, key: '_Ask-Answer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AskAnswer::Application.config.session_store :active_record_store
